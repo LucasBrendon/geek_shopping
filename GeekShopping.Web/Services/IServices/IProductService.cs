35e16109fa053120edc0b4731f0f5d2adb25614f
Lucas Brendon <65317316+LucasBrendon@users.noreply.github.com>
2022-05-20 21:49:09 -0300

    Criação de projeto web

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using GeekShopping.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> Create(ProductModel model);
+        Task<ProductModel> Update(ProductModel model);
+        Task<bool> Delete(long id);
+
+    }
+}
